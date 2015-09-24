ce52044d1d677b538bf55aecbc3b4edf37d7e58d
malnick <malnick@gmail..com>
2015-09-24 08:59:11 -0700

    plot utils

---
+++ plot.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/malnick/plot"
+	"github.com/malnick/plot/plotter"
+	"github.com/malnick/plot/plotutil"
+	"github.com/malnick/plot/vg"
+)
+
+func plotMemory() {
+
+}
