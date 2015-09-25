97076fc10b19d93c3481a1942387508cfcdcf653
malnick <malnick@gmail..com>
2015-09-25 11:25:28 -0700

    influx

---
+++ influx.go
@@ -0,0 +1,84 @@
+package main
+
+import "github.com/influxdb/influxdb/client"
+import (
+	"fmt"
+	log "github.com/Sirupsen/logrus"
+	"net/url"
+	//"os"
+)
+
+const (
+	InfluxHost       = "localhost"
+	InfluxPort       = 8086
+	InfluxDb         = "hosts"
+	HostMeasurements = "shapes"
+)
+
+func SetInflux() *client.Client {
+	u, err := url.Parse(fmt.Sprintf("http://%s:%d", InfluxHost, InfluxPort))
+	if err != nil {
+		log.Error("Unable to parse url ", u)
+		log.Fatal(err)
+	}
+
+	conf := client.Config{
+		URL: *u,
+		// Will have database auth later
+		//Username: os.Getenv("INFLUX_USER"),
+		//Password: os.Getenv("INFLUX_PWD"),
+	}
+
+	con, err := client.NewClient(conf)
+	if err != nil {
+		log.Error("Unable to create new client ", con)
+		log.Fatal(err)
+	}
+
+	dur, ver, err := con.Ping()
+	if err != nil {
+		log.Error("Unable to run con.Ping() ", dur, " ", ver)
+		log.Fatal(err)
+	}
+	log.Printf("Influx OK - response time %v, version %s", dur, ver)
+	return con
+
+}
+
+func queryDB(con *client.Client, cmd string, db string) (res []client.Result, err error) {
+	log.Debug("Influx Query: ", cmd)
+	q := client.Query{
+		Command:  cmd,
+		Database: db,
+	}
+	if response, err := con.Query(q); err == nil {
+		if response.Error() != nil {
+			return res, response.Error()
+		}
+		res = response.Results
+	}
+	return
+}
+
+func CheckDb(con *client.Client, db string) error {
+	_, err := queryDB(con, fmt.Sprintf("CREATE DATABASE %s", db), db)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func dumpToInflux(host string, data []*HttpPost) (response string, err error) {
+	log.Debug(fmt.Sprintf("%s: %s", host, data))
+	// Create an influx client
+	influxClient := SetInflux()
+	// Check to make sure the DB is created, and create it if not
+	err = CheckDb(influxClient, InfluxDb)
+	if err != nil {
+		log.Warn(fmt.Sprintf("Database %s: %s", InfluxDb, err))
+	}
+	// Influxify the JSON
+	influxData := influxify(data)
+	// Dump the data
+	return "this", nil
+}
