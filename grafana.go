6585f9d376ecc3ee68431a9ff4c89edcee1e7d3f
malnick <malnick@gmail..com>
2015-10-01 14:43:10 -0700

    kata

---
+++ grafana.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"text/template"
+)
+
+type HostDashboard struct {
+	Hostname string
+}
+
+func createHostDashboard() {
+
+}
+
+func updateHostDashboard() {
+
+}
