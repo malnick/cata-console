d86d3093e6969ac398fe6038e5316ff64c942a20
malnick <malnick@gmail..com>
2015-10-02 10:50:27 -0700

    tons of config

--- config.go
+++ config.go
@@ -17,16 +17,33 @@ type Alarm struct {
 }
 
 type Config struct {
-	LogLevel    string  `json:"log_level"`
-	Alarms      []Alarm `json:"alarms"`
-	GrafanaUrl  string  `json:"grafana_url"`
-	GrafanaAuth string  `json:"grafana_auth"`
-	KataHome    string  `json:"kata_home"`
+	LogLevel string  `json:"log_level"`
+	Alarms   []Alarm `json:"alarms"`
+	KataHome string  `json:"kata_home"`
+	// Grafana configuration
+	GrafanaUrl  string `json:"grafana_url"`
+	GrafanaAuth string `json:"grafana_auth"`
+	// Influx datasource config for grafana
+	InfluxUrl               string `json:"influx_url"`
+	InfluxPort              string `json:"influx_port"`
+	InfluxUser              string `json:"influx_user"`
+	InfluxPassword          string `json:"influx_password"`
+	InfluxBasicAuthEnabled  string `json:"influx_basic_auth_enabled"`
+	InfluxBasicAuthUser     string `json:"influx_basic_auth_user"`
+	InfluxBasicAuthPassword string `json:"influx_basic_auth_password"`
 }
 
+// Default Configuration
 var DefaultGrafanaUrl = "localhost:3000"
 var DefaultGrafanaAuth = "KATA_GRAFANA_AUTH Not Set!"
 var DefaultKataHome = fmt.Sprintf("%s/.kata", os.Getenv("HOME"))
+var DefaultInfluxUrl = "localhost"
+var DefaultInfluxPort = "8086"
+var DefaultInfluxUser = "admin"
+var DefaultInfluxPassword = "admin"
+var DefaultInfluxBasicAuthEnabled = "false"
+var DefaultInfluxBasicAuthUser = ""
+var DefaultInfluxBasicAuthPassword = ""
 
 func ParseEnv(c Config) Config {
 	// Create a few matches for our env parsing down the road
@@ -37,11 +54,32 @@ func ParseEnv(c Config) Config {
 	matchGrafanaAuth, _ := regexp.Compile("KATA_GRAFANA_AUTH=*")
 	// Get home kata
 	matchKataHome, _ := regexp.Compile("KATA_HOME=*")
+	// Get influx url
+	matchInfluxUrl, _ := regexp.Compile("KATA_INFLUX_URL=*")
+	// Get influx port
+	matchInfluxPort, _ := regexp.Compile("KATA_INFLUX_PORT=*")
+	// Get influx user
+	matchInfluxUser, _ := regexp.Compile("KATA_INFLUX_USER=*")
+	// Get influx password
+	matchInfluxPassword, _ := regexp.Compile("KATA_INFLUX_PASSWORD=*")
+	// Get influx basic auth enabled
+	matchInfluxBasicAuthEnabled, _ := regexp.Compile("KATA_INFLUX_BASIC_AUTH_ENABLED=*")
+	// Get influx basic auth user
+	matchInfluxBasicAuthUser, _ := regexp.Compile("KATA_INFLUX_BASIC_AUTH_USER=*")
+	// Get influx basic auth password
+	matchInfluxBasicAuthPassword, _ := regexp.Compile("KATA_INFLUX_BASIC_AUTH_PASSWORD=*")
 
 	// Set the defaults and override later
 	c.GrafanaUrl = DefaultGrafanaUrl
 	c.GrafanaAuth = DefaultGrafanaAuth
 	c.KataHome = DefaultKataHome
+	c.InfluxUrl = DefaultInfluxUrl
+	c.InfluxPort = DefaultInfluxPort
+	c.InfluxUser = DefaultInfluxUser
+	c.InfluxPassword = DefaultInfluxPassword
+	c.InfluxBasicAuthEnabled = DefaultInfluxBasicAuthEnabled
+	c.InfluxBasicAuthUser = DefaultInfluxBasicAuthUser
+	c.InfluxBasicAuthPassword = DefaultInfluxBasicAuthPassword
 
 	// Parse the env for our config
 	for _, e := range os.Environ() {
@@ -69,6 +107,34 @@ func ParseEnv(c Config) Config {
 		if matchKataHome.MatchString(e) {
 			newKataHome := strings.Split(e, "=")[1]
 			c.KataHome = newKataHome
+		}
+		if matchInfluxUrl.MatchString(e) {
+			newInfluxUrl := strings.Split(e, "=")[1]
+			c.InfluxUrl = newInfluxUrl
+		}
+		if matchInfluxPort.MatchString(e) {
+			newInfluxPort := strings.Split(e, "=")[1]
+			c.InfluxPort = newInfluxPort
+		}
+		if matchInfluxUser.MatchString(e) {
+			newInfluxUser := strings.Split(e, "=")[1]
+			c.InfluxUser = newInfluxUser
+		}
+		if matchInfluxPassword.MatchString(e) {
+			newInfluxPassword := strings.Split(e, "=")[1]
+			c.InfluxPassword = newInfluxPassword
+		}
+		if matchInfluxBasicAuthEnabled.MatchString(e) {
+			influxBasicAuthEnabled := strings.Split(e, "=")[1]
+			c.InfluxBasicAuthEnabled = influxBasicAuthEnabled
+		}
+		if matchInfluxBasicAuthUser.MatchString(e) {
+			influxBasicAuthUser := strings.Split(e, "=")[1]
+			c.InfluxBasicAuthUser = influxBasicAuthUser
+		}
+		if matchInfluxBasicAuthPassword.MatchString(e) {
+			influxBasicAuthPassword := strings.Split(e, "=")[1]
+			c.InfluxBasicAuthPassword = influxBasicAuthPassword
 		}
 	}
 	checkhome(c.KataHome)
